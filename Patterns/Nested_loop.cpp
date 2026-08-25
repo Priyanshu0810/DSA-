61dca15a488b10a9a5b83ac4fe3b3e37a8c04b28
Priyanshu kumar <153292007+Priyanshu0810@users.noreply.github.com>
2026-08-25 22:32:47 +0530

    Added new folder

---
+++ Patterns/Nested_loop.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter the no. \n";
+    cin>>n;
+    for(int x=1;x<=n;x++){
+        for(int i=1;i<=n;i++){
+           cout<<x;
+        } cout<<endl;}
+        return 0;
+}
