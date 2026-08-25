2b990d625946dc6d5ed26fec2e76b40792c02289
Priyanshu kumar <153292007+Priyanshu0810@users.noreply.github.com>
2026-08-25 23:44:02 +0530

    Added Star pattern

---
+++ Star.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "enter the no. \n";
+    cin >> n;
+    for (int x = 1; x <= n; x++)
+    {
+        for (int i = 1; i <= x; i++)
+        {
+            cout << x << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
