cafddd772cec86fcdaecea6951e0d3a894116a62
Priyanshu kumar <153292007+Priyanshu0810@users.noreply.github.com>
2026-08-25 23:40:41 +0530

    Updated DSA

--- Patterns/Nested_loop.cpp
+++ Patterns/Nested_loop.cpp
@@ -1,11 +1,11 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int n;
-    cout<<"enter the no. \n";
-    cin>>n;
-    for(int x=1;x<=n;x++){
-        for(int i=1;i<=n;i++){
+    int n;                               // when n = 4 the output is
+    cout<<"enter the no. \n";            // 1 1 1 1
+    cin>>n;                              // 2 2 2 2
+    for(int x=1;x<=n;x++){               // 3 3 3 3
+        for(int i=1;i<=n;i++){           // 4 4 4 4
            cout<<x;
         } cout<<endl;}
         return 0;
