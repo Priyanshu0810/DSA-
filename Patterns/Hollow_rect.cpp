32a3ede018f9366c2668a61fa4ab926d5b69f74b
Priyanshu kumar <153292007+Priyanshu0810@users.noreply.github.com>
2026-08-30 23:05:43 +0530

    Add files via upload

---
+++ Patterns/Hollow_rect.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter tha No \n";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        cout << "*"; // First
+        for (int j = 1; j <= n - 1; j++)
+        {
+            if (i == 1 || i == n)
+            {
+                cout << "*";
+            }
+            else
+            {
+                cout << " ";
+            }
+        }
+        cout << "*" << endl; // last
+    }
+    return 0;
+}
